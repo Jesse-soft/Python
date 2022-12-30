76e92fa6b0201952ad7085715d2c2ed06a0df95c
Jesse-Soft <onyedirejesse@gmail.com>
2022-12-31 00:44:15 +0100

    new commit

---
+++ hello_world/functions.py
@@ -0,0 +1,16 @@
+# functions are set of instructions that take an input and returns an outputs.
+
+# def <function name> (<params>):
+#     <task to complete>
+
+def sum(x,y):
+    return x+y
+
+
+currentYear=input("current year ")
+age=input("age ")
+currentYear=int(currentYear)
+age = int(age)
+def calDateOfBirth(Year,yage):
+    return (Year-yage)    
+print(calDateOfBirth(currentYear,age))    
