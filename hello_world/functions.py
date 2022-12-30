c96903b8fd99ceb8784409b93b600d0f74a48de3
Jesse-Soft <onyedirejesse@gmail.com>
2022-12-31 00:44:53 +0100

    new commit

--- hello_world/functions.py
+++ hello_world/functions.py
@@ -3,14 +3,18 @@
 # def <function name> (<params>):
 #     <task to complete>
 
-def sum(x,y):
+def sum(x, y):
     return x+y
 
 
-currentYear=input("current year ")
-age=input("age ")
-currentYear=int(currentYear)
+currentYear = input("current year ")
+age = input("age ")
+currentYear = int(currentYear)
 age = int(age)
-def calDateOfBirth(Year,yage):
-    return (Year-yage)    
-print(calDateOfBirth(currentYear,age))    
+
+
+def calDateOfBirth(Year, yage):
+    return (Year-yage)
+
+
+print(calDateOfBirth(currentYear, age))
